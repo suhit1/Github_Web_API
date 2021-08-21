8d8ef30e19b2912ed1b48abce7ad2afa32956aa7
Suhit Gupta <suhitgupta111@gmail.com>
2021-08-21 14:27:58 +0530

    not_found Updated

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ let followers = document.getElementById("followers");
 let following = document.getElementById("following");
 let repos = document.getElementById("repos");
 let repo_link = document.getElementById("repo_link");
+let not_found = document.getElementById("not_found");
 
 btn.disabled = true;
 
@@ -21,12 +22,12 @@ username.addEventListener("input", function () {
   } else {
     btn.disabled = true;
     lower_div.style.display = "none";
+    not_found.style.display = "none";
   }
 });
 
 btn.addEventListener("click", function (event) {
   event.preventDefault();
-  lower_div.style.display = "block";
 
   let get_request = new XMLHttpRequest();
 
@@ -34,49 +35,54 @@ btn.addEventListener("click", function (event) {
 
   get_request.addEventListener("load", function (event) {
     let data = JSON.parse(event.target.responseText);
-    console.log(data);
+    console.log(typeof data);
 
-    username_image.setAttribute("src", data[0].owner.avatar_url);
-    repos.innerText = `Repos: ${data.length}`;
-    username_name.innerText = `(@${data[0].owner.login})`;
-    username_name.setAttribute("href", `${data[0].owner.html_url}`);
-    username_name.setAttribute("target", "_blank");
+    if (data.length === 0) {
+      not_found.style.display = "block";
+    } else {
+      not_found.style.display = "none";
+      lower_div.style.display = "block";
+      username_image.setAttribute("src", data[0].owner.avatar_url);
+      repos.innerText = `Repos: ${data.length}`;
+      username_name.innerText = `(@${data[0].owner.login})`;
+      username_name.setAttribute("href", `${data[0].owner.html_url}`);
+      username_name.setAttribute("target", "_blank");
 
-    repo_link.innerHTML = "";
+      repo_link.innerHTML = "";
 
-    // Repository links
+      // Repository links
 
-    data.forEach((element) => {
-      let list_item = document.createElement("li");
+      data.forEach((element) => {
+        let list_item = document.createElement("li");
 
-      let a = document.createElement("a");
+        let a = document.createElement("a");
 
-      a.innerText = element.name;
-      list_item.setAttribute("class", "list_style");
-      a.setAttribute("href", `${element.html_url}`);
-      a.setAttribute("class", "anchor");
-      a.setAttribute("target", "_blank");
-      list_item.appendChild(a);
-      repo_link.appendChild(list_item);
-    });
+        a.innerText = element.name;
+        list_item.setAttribute("class", "list_style");
+        a.setAttribute("href", `${element.html_url}`);
+        a.setAttribute("class", "anchor");
+        a.setAttribute("target", "_blank");
+        list_item.appendChild(a);
+        repo_link.appendChild(list_item);
+      });
 
-    // Getting Name Of github Username,followers count and following count
+      // Getting Name Of github Username,followers count and following count
 
-    let name_github = new XMLHttpRequest();
-    name_github.addEventListener("load", function (event) {
-      let data = JSON.parse(event.target.responseText);
-      console.log(data);
-      if ("name" in data) nameOn_github.innerText = data.name;
-      console.log(data.name);
-      followers.innerText = `Followers: ${data.followers}`; // followers count
-      following.innerText = `Following: ${data.following}`; // following count
-    });
+      let name_github = new XMLHttpRequest();
+      name_github.addEventListener("load", function (event) {
+        let data = JSON.parse(event.target.responseText);
+        console.log(data);
+        if ("name" in data) nameOn_github.innerText = data.name;
+        console.log(data.name);
+        followers.innerText = `Followers: ${data.followers}`; // followers count
+        following.innerText = `Following: ${data.following}`; // following count
+      });
 
-    name_github.open("GET", `https://api.github.com/users/${username.value}`);
+      name_github.open("GET", `https://api.github.com/users/${username.value}`);
 
-    name_github.send();
+      name_github.send();
+    }
   });
-
   get_request.open(
     "GET",
     `https://api.github.com/users/${username.value}/repos`
